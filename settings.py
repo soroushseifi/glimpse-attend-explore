06acc7b9664df24ad34a0cc2c086bbe81c98dbf8
soroushseifi <45103733+soroushseifi@users.noreply.github.com>
2021-10-13 22:28:46 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,7 +9,6 @@ import torch as t
 t.set_default_tensor_type('torch.cuda.FloatTensor')
 channels=3
 nglimpses=8
-nclasses=10
 glimpse_size=48
 retina_size=glimpse_size//3
 checkpoint_iteration=2
@@ -17,7 +16,6 @@ batch_size=2
 iters=300
 img_size_height=128
 img_size_width=256
-temperature=0.1
 #we have 3 different scales in the retina with different resolutions, therefore we allow overlapping glimpses excepet for th inside 1/3 of the glimpse which is taken in full resolution
 nb_height_patches=img_size_height//(glimpse_size//3)
 nb_width_patches=img_size_width//(glimpse_size//3)
