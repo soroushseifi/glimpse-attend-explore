9f9e6450ecb339c3fe64c82e8d6432e1196b50cb
soroushseifi <45103733+soroushseifi@users.noreply.github.com>
2021-10-13 22:13:32 +0200

    Update attention.py

--- attention.py
+++ attention.py
@@ -12,7 +12,6 @@ class ResidualAdd(t.nn.Module):
         x=x+res
         return x
 class Attention_Module(nn.Module):
-    #initialize memory matrices
     def __init__(self,n_channels: int=channels,b_reduced_depth:int=16,b_height:int=img_size_height//16,b_width:int=img_size_width//16):
         super().__init__()
         self.fc_attention=nn.Linear(b_reduced_depth*(b_height)*(b_width),b_height*b_width)
@@ -50,26 +49,40 @@ class Attention_Module(nn.Module):
             nn.Conv2d(64,channels,3,padding=1,bias=False),
             nn.ReLU()
         )
-    def forward(self,reduced_bottleneck,inpainted_bottleneck,mem_3,mem_2,mem_1):
-        bottleneck_attention=self.fc_attention(reduced_bottleneck).view([batch_size,1,nb_height_patches,nb_width_patches])
-        inpainted_bottleneck_att=bottleneck_attention*inpainted_bottleneck
+    def forward(self,network):
+        network.bn_attention=F.relu(self.fc_attention(network.universal_features_reduced_raw)).view([batch_size,1,nb_height_patches,nb_width_patches])
+        inpainted_bottleneck_att=network.bn_attention*network.bn_pred
         deconv_3=F.relu(self.bn_deconv_b(self.deconv_b(inpainted_bottleneck_att)))
-        deconv_3_skip=t.cat([deconv_3,mem_3],1)
+        deconv_3_skip=t.cat([deconv_3,network.m3],1)
         merge_3=self.merge_3(deconv_3_skip)
         merge_3_feat=F.relu(self.bn_merge_3(merge_3[:,:128,:,:]))
         merge_3_att=t.unsqueeze(F.relu(merge_3[:,128,:,:]),1)
-        level_3=merge_3_feat*merge_3_att
-        deconv_2=F.relu(self.bn_deconv_2(self.deconv_2(level_3)))
-        deconv_2_skip=t.cat([deconv_2,mem_2],1)
+        l3_pred_att=merge_3_att*merge_3_feat
+        deconv_2=F.relu(self.bn_deconv_2(self.deconv_2(l3_pred_att)))
+        deconv_2_skip=t.cat([deconv_2,network.m2],1)
         merge_2=self.merge_2(deconv_2_skip)
         merge_2_feat=F.relu(self.bn_merge_2(merge_2[:,:64,:,:]))
         merge_2_att=t.unsqueeze(F.relu(merge_2[:,64,:,:]),1)
         level_2=merge_2_feat*merge_2_att
         deconv_1=self.deconv_1(level_2)
-        deconv_1_skip=t.cat([deconv_1,mem_1],1)
+        deconv_1_skip=t.cat([deconv_1,network.m1],1)
         merge_1=self.merge_1(deconv_1_skip)
         merge_1_feat=F.relu(self.bn_merge_1(merge_1[:,:64,:,:]))
         merge_1_att=t.unsqueeze(F.relu(merge_1[:,64,:,:]),1)
         level_1=merge_1_feat*merge_1_att
-        pred=self.pred(level_1)
-        return pred,bottleneck_attention
+        network.att_pred=self.pred(level_1)
+        return
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
