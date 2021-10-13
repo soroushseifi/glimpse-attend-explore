e5655569ec19be93e21e24f39218d9b3c2a790e6
soroushseifi <45103733+soroushseifi@users.noreply.github.com>
2021-10-13 22:14:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,34 @@
 # coding: utf-8
 
 # In[ ]:
+
+
 import torch as t
-from utils import *
-from gae import GAE
-#Set folders for storing results
+import numpy as np
+import os
+import cv2
+import copy
+import torch.nn as nn
+from settings import *
+from preprocessing import get_data,set_directories,load_checkpoint
+from network import model
+from traineval import *
+#from collections import namedtuple
 set_directories()
-#get a list of train and test images 
-images,test_images=get_data()
-#find number of training and test batches
+images,test_images=get_data(1)
 nb_batches=len(images)//batch_size
 nb_test_batches=len(test_images)//batch_size
-#get a generator for test and training imafes
-generator_train=generate_data(images)
-generator_test=generate_data(test_images)
-#load from the checkpoint if it exists orthewise instantiate the model
-model=GAE()
-model,start_epoch,results_train,results_test=load_model(model)
-model.cuda()
-for epoch in range(start_epoch,iters):
-    last_batch=False
-    model.train()
-    for batch in range(nb_batches):
-        if batch==nb_batches-1:
-            last_batch=True
-        train_images=generate_batch(generator_train)
-        train_images=t.tensor(train_images).float().permute([0,3,1,2]).cuda()
-        model(train_images,epoch,batch,results_train,last_batch,is_training=True,save_outputs=True,save_all=False)
-    last_batch=False
-    with t.no_grad():
-        model.eval()
-        for test_batch in range(nb_test_batches):
-            if test_batch==nb_test_batches-1:
-                last_batch=True
-            test_images=generate_batch(generator_test)
-            test_images=t.tensor(test_images).float().permute([0,3,1,2]).cuda()
-            model(test_images,epoch,test_batch,results_test,last_batch,is_training=False,save_outputs=True,save_all=False)
+segmentit=model().cuda()
+best_epoch,best,results_train,results_train_test=load_checkpoint(segmentit)
+start=copy.deepcopy(best_epoch)
+best=t.ones(1)*best
+for i in range(start,iters):
+    train(segmentit,i,images,nb_batches,results_train,best,best_epoch)
+    best,best_epoch=evaluate(segmentit,i,test_images,nb_test_batches,results_train_test,best,best_epoch)
+
+
+# In[ ]:
+
+
+
+
